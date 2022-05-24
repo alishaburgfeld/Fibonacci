f46cdc7ee8e3ac4087b6d74f2dff907c05783a9f
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-24 15:58:53 -0400

    added JS File

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,28 @@
+// const fibonacci = (num) => {
+//     let arr=[0,1]
+//     for (let i=0;i<num;i++) {
+//         arr.push((arr[i]+arr[i+1]))
+//     }
+//     return arr[num]
+// }
+
+// // console.log(fibonacci(7))
+// module.exports = {fibonacci}
+
+
+//Recursion example:
+
 const fibonacci = (num) => {
-    
+    if (num===1) {
+        return 1
+    }
+    if (num===2) {
+        return 1
+    }
+    return fibonacci(num-1) + fibonacci(num-2)
 }
 
-module.exports = {fibonacci}
+console.log(fibonacci(7))
+// module.exports = {fibonacci}
+
+
